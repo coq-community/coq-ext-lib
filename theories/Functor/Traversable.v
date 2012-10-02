667175c8888735738aa4b4ed5a6f8b1b90f82ced
gregory malecha <gmalecha@cs.harvard.edu>
2012-10-02 15:19:27 -0400

    adding option instance for traversable.

--- theories/Functor/Traversable.v
+++ theories/Functor/Traversable.v
@@ -4,7 +4,7 @@ Require Import Monad.
 Set Implicit Arguments.
 Set Maximal Implicit Insertion.
 
-Class Traversable T :=
+Class Traversable (T : Type -> Type) : Type :=
 { mapT : forall {F} {Ap:Applicative F} {A B}, (A -> F B) -> T A -> F (T B) }.
 
 Section traversable.
@@ -14,3 +14,10 @@ Section traversable.
   Definition forT {A B} (aT:T A) (f:A -> F B) : F (T B) := mapT f aT.
 End traversable.
 
+Global Instance Traversable_option : Traversable option :=
+{| mapT := fun F _ _ _ f o =>
+  match o with
+    | None => pure None
+    | Some o => ap (pure (@Some _)) (f o)
+  end
+|}.
