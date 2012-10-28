4c726990efe727e890a3cad18d24a93e855d9e4a
gregory malecha <gmalecha@cs.harvard.edu>
2012-10-28 17:20:06 -0400

    working on Reducible.

--- theories/Structures/Reducible.v
+++ theories/Structures/Reducible.v
@@ -1,14 +1,19 @@
 Require Import RelationClasses.
 Require Import ExtLib.Structures.BinOps.
 
-Class Container (T E : Type) : Type :=
-  In : T -> E -> Prop.
-
 Class Reducible (T E : Type) : Type :=
   reduce : forall {A} (base : A) (single : E -> A) (join : A -> A -> A), T -> A.
 
 Class Foldable (T E : Type) : Type :=
   fold : forall {A} (add : E -> A -> A) (base : A), T -> A.
+
+Section RedFold.
+  Variables T E : Type.
+  
+  Definition Reducible_from_Foldable (R : Foldable T E) : Reducible T E :=
+    fun A base single join =>
+      @fold _ _ R A (fun x => join (single x)) base.
+End RedFold.
 
 Section foldM.
   Require Import ExtLib.Structures.Monad.
@@ -20,6 +25,26 @@ Section foldM.
   Definition foldM {A} (add : E -> A -> m A) (base : m A) (t : T) : m A :=
     fold (fun x acc => bind acc (add x)) base t.  
 End foldM.
+
+
+(*
+Section mapping.
+  Variables T E : Type.
+  Variables U V : Type.
+  
+  Variable f : E -> V.
+
+  Variable Red_te : Reducible T E. 
+  Variable Ctor_uv : Constructable U V. (* [unit], [inj], [plus] *)
+
+
+  reduce unit (fun x => inj (f x)) plus.
+*)  
+  
+
+
+
+
 
 
 
