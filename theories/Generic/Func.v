cc33945ce8a51fe685d7aea8a6dda738b2bc36d2
gregory malecha <gmalecha@cs.harvard.edu>
2013-01-10 14:48:16 -0500

    adding helper functions for Generic/Func.

--- theories/Generic/Func.v
+++ theories/Generic/Func.v
@@ -20,3 +20,21 @@ Fixpoint applyF {domain : list Type} {range : Type}
     | nil => fun x _ => x
     | d :: ds => fun f x_xs => applyF (f (fst x_xs)) (snd x_xs)
   end.
+
+Fixpoint const {D R} (r : R) : asFunc D R :=
+  match D with
+    | nil => r
+    | _ :: D => fun _ => const r
+  end.
+
+Fixpoint uncurry {D R} {struct D} : (asTuple D -> R) -> asFunc D R :=
+  match D as D return (asTuple D -> R) -> asFunc D R with
+    | nil => fun x => x tt
+    | d :: D => fun f d => uncurry (fun x => f (d, x))
+  end.
+
+Fixpoint curry {D R} {struct D} : asFunc D R -> (asTuple D -> R) :=
+  match D as D return asFunc D R -> (asTuple D -> R) with
+    | nil => fun x _ => x
+    | d :: D => fun f x => curry (f (fst x)) (snd x)
+  end.
