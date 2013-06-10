d151ea4fb0a4db3a309c855212504f0b0dd447bb
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-06-09 23:00:33 -0400

    fixed the definition of CoFunctor and added instances to Fun
    - These instances should really go in Functor and CoFunctor
      but some of them require both.
      - Maybe there should be a single file with Functor and CoFunctor.

--- theories/Data/Fun.v
+++ theories/Data/Fun.v
@@ -4,6 +4,7 @@ Require Import ExtLib.Core.Type.
 Require Import ExtLib.Data.PreFun.
 Require Import ExtLib.Structures.Proper.
 Require Import ExtLib.Structures.Functor.
+Require Import ExtLib.Structures.CoFunctor.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -11,8 +12,24 @@ Set Strict Implicit.
 Section functors.
   Variable A : Type.
 
-  Instance Functor_Fun : Functor (Fun A) :=
+  Local Instance Functor_Fun : Functor (Fun A) :=
   { fmap _A _B g f x := g (f x) }.
+
+  Local Instance CoFunctor_Fun T : CoFunctor (fun x => x -> T) :=
+  {| cofmap := fun _ _ g f => fun x => f (g x) |}.
+
+  Local Instance Functor_functor F G (fF : Functor F) (fG : Functor G) : Functor (fun x => F (G x)) :=
+  {| fmap := fun _ _ g => @fmap F _ _ _ (@fmap G _ _ _ g) |}.
+
+  Local Instance CoFunctor_functor F G (fF : Functor F) (fG : CoFunctor G) : CoFunctor (fun x => F (G x)) :=
+  {| cofmap := fun _ _ g => @fmap F _ _ _ (@cofmap G _ _ _ g) |}.
+
+  Local Instance Functor_cofunctor F G (fF : CoFunctor F) (fG : Functor G) : CoFunctor (fun x => F (G x)) :=
+  {| cofmap := fun _ _ g => @cofmap F _ _ _ (@fmap G _ _ _ g) |}.
+
+  Local Instance CoFunctor_cofunctor F G (fF : CoFunctor F) (fG : CoFunctor G) : Functor (fun x => F (G x)) :=
+  {| fmap := fun _ _ g => @cofmap F _ _ _ (@cofmap G _ _ _ g) |}.
+
 
 End functors.
 
--- theories/Structures/CoFunctor.v
+++ theories/Structures/CoFunctor.v
@@ -7,20 +7,20 @@ Section functor.
   Variable F : Type -> Type.
 
   Class CoFunctor : Type :=
-  { cofmap : forall A B, (A -> B) -> F A -> F B }.
+  { cofmap : forall A B, (B -> A) -> F A -> F B }.
 
   Definition ID {T : Type} (f : T -> T) : Prop :=
     forall x, f x = x.
 
   Class CoPFunctor : Type :=
   { CoFunP : Type -> Type
-  ; copfmap : forall {A B} {P : CoFunP B}, (A -> B) -> F A -> F B
+  ; copfmap : forall {A B} {P : CoFunP B}, (B -> A) -> F A -> F B
   }.
 
   Existing Class CoFunP.
   Hint Extern 0 (@CoFunP _ _ _) => progress (simpl CoFunP) : typeclass_instances.
 
-  Global Instance PFunctor_From_Functor (F : CoFunctor) : CoPFunctor :=
+  Global Instance CoPFunctor_From_CoFunctor (F : CoFunctor) : CoPFunctor :=
   { CoFunP := Any
   ; copfmap := fun _ _ _ f x => cofmap f x
   }.
