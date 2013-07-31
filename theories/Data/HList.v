da26c427f56ec78e50d2048aeaf89de357576f25
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-07-31 17:35:26 -0400

    adding a few new instances for HList.

--- theories/Data/HList.v
+++ theories/Data/HList.v
@@ -1,8 +1,7 @@
-Require Import List.
-Require Import ExtLib.Structures.EqDep.
 Require Import Coq.Lists.List.
 Require Import Relations RelationClasses.
-Require Import Core.Type.
+Require Import ExtLib.Core.Type.
+Require Import ExtLib.Core.RelDec.
 Require Import ExtLib.Data.SigT.
 Require Import ExtLib.Structures.Proper.
 Require Import ExtLib.Structures.EqDep.
@@ -60,25 +59,28 @@ Section hlist.
     | hlist_eqv_nil : equiv_hlist Hnil Hnil
     | hlist_eqv_cons : forall l ls x y h1 h2, eqv x y -> equiv_hlist h1 h2 ->
       @equiv_hlist (l :: ls) (Hcons x h1) (Hcons y h2).
-    
-    Global Instance Reflexive_equiv_hlist (R : forall t, Reflexive (@eqv t)) ls : Reflexive (@equiv_hlist ls).
+
+    Global Instance Reflexive_equiv_hlist (R : forall t, Reflexive (@eqv t)) ls
+    : Reflexive (@equiv_hlist ls).
     Proof.
       red. induction x; constructor; auto. reflexivity.
     Qed.
 
-    Variable ED : EquivDec.EqDec _ (@eq iT).    
-    
-    Global Instance Symmetric_equiv_hlist (R : forall t, Symmetric (@eqv t)) ls : Symmetric (@equiv_hlist ls).
+    Variable ED : EquivDec.EqDec _ (@eq iT).
+
+    Global Instance Symmetric_equiv_hlist (R : forall t, Symmetric (@eqv t)) ls
+    : Symmetric (@equiv_hlist ls).
     Proof.
       red. induction x; intros; rewrite (hlist_eta y) in *; constructor; auto.
       inversion H; subst. inv_all. subst. symmetry; auto.
       eapply IHx. inversion H; auto. inv_all. subst. auto.
     Qed.
 
-    Global Instance Transitive_equiv_hlist (R : forall t, Transitive (@eqv t)) ls : Transitive (@equiv_hlist ls).
+    Global Instance Transitive_equiv_hlist (R : forall t, Transitive (@eqv t)) ls
+    : Transitive (@equiv_hlist ls).
     Proof.
-      red. induction x; intros; rewrite (hlist_eta y) in *; rewrite (hlist_eta z) in *; 
-      try solve [ constructor; auto ].
+      red. induction x; intros; rewrite (hlist_eta y) in *;
+           rewrite (hlist_eta z) in *; try solve [ constructor; auto ].
       inversion H; clear H; subst. inversion H0; clear H0; subst.
       inv_all; subst. constructor; try etransitivity; eauto.
     Qed.
@@ -96,6 +98,33 @@ Section hlist.
     intros; rewrite (hlist_eta h); reflexivity.
   Qed.
 
+  Section injection.
+    Variable rd : RelDec (@eq iT).
+    Variable rdc : RelDec_Correct rd.
+
+    Global Instance Injection_hlist_cons ls t (a : F t) (b : hlist ls) c d
+    : Injective (Hcons a b = Hcons c d) :=
+      { result := a = c /\ b = d }.
+    Proof.
+      abstract (inversion 1; eapply inj_pair2 in H2; eapply inj_pair2 in H1; auto).
+    Defined.
+
+    Theorem equiv_eq_eq : forall ls (x y : hlist ls),
+                            equiv_hlist (fun x => @eq _) x y <-> x = y.
+    Proof.
+      induction x; simpl; intros.
+      { split. inversion 1. rewrite hlist_nil_eta. reflexivity.
+        intros; subst; constructor. }
+      { split.
+        { inversion 1; subst.
+          apply inj_pair2 in H2.
+          apply inj_pair2 in H5.
+          apply inj_pair2 in H4. subst. f_equal. eapply IHx. eauto. }
+        { intros; subst. constructor; auto.
+          reflexivity. } }
+    Qed.
+  End injection.
+
   Inductive member (a : iT) : list iT -> Type :=
   | MZ : forall ls, member a (a :: ls)
   | MN : forall l ls, member a ls -> member a (l :: ls).
@@ -106,7 +135,7 @@ Section hlist.
       | MN _ _ r => fun hl => hlist_get r (hlist_tl hl)
     end.
 
-  Fixpoint hlist_nth_error {ls} (hs : hlist ls) (n : nat) 
+  Fixpoint hlist_nth_error {ls} (hs : hlist ls) (n : nat)
     : option (match nth_error ls n with
                 | None => unit
                 | Some x => F x
@@ -117,7 +146,7 @@ Section hlist.
                                         end)
       with
       | Hnil => None
-      | Hcons l ls h hs => 
+      | Hcons l ls h hs =>
         match n as n return option (match nth_error (l :: ls) n with
                                       | None => unit
                                       | Some x => F x
@@ -133,7 +162,7 @@ Section hlist.
       | None => unit
       | Some t => F t
     end :=
-    match h in hlist ls , n as n 
+    match h in hlist ls , n as n
       return match nth_error ls n with
                | None => unit
                | Some t => F t
@@ -144,18 +173,20 @@ Section hlist.
       | Hcons _ _ x _ , 0 => x
       | Hcons _ _ _ h , S n => hlist_nth h n
     end.
-  
+
   Require Import ExtLib.Data.ListNth.
-  Definition cast1 T (l l' : list T) n v := 
+  Definition cast1 T (l l' : list T) n v :=
     (fun pf : nth_error l n = Some v => eq_sym (nth_error_weaken l' l n pf)).
   Definition cast2 T (l l' : list T) n :=
-    (fun pf : nth_error l n = None => eq_sym (@nth_error_app_R _ l l' _ (nth_error_length_ge _ _ pf))).
+    (fun pf : nth_error l n = None =>
+       eq_sym (@nth_error_app_R _ l l' _ (nth_error_length_ge _ _ pf))).
 
   Require Import ExtLib.Tactics.EqDep.
-  Require Import ExtLib.Data.Option.  
+  Require Import ExtLib.Data.Option.
 
-  Theorem hlist_nth_hlist_app (e : EquivDec.EqDec iT (@eq iT)) : forall l l' (h : hlist l) (h' : hlist l') n,
-    hlist_nth (hlist_app h h') n = 
+  Theorem hlist_nth_hlist_app (e : EquivDec.EqDec iT (@eq iT))
+  : forall l l' (h : hlist l) (h' : hlist l') n,
+    hlist_nth (hlist_app h h') n =
     match nth_error l n as k
       return nth_error l n = k ->
       match nth_error (l ++ l') n with
@@ -163,9 +194,9 @@ Section hlist.
         | Some t => F t
       end
       with
-      | Some _ => fun pf => 
+      | Some _ => fun pf =>
         match cast1 _ _ _ pf in _ = z ,
-          eq_sym pf in _ = w 
+          eq_sym pf in _ = w
           return match w with
                    | None => unit
                    | Some t => F t
@@ -177,13 +208,13 @@ Section hlist.
           with
           | eq_refl , eq_refl => fun x => x
         end (hlist_nth h n)
-      | None => fun pf => 
-        match cast2 _ _ _ pf in _ = z 
+      | None => fun pf =>
+        match cast2 _ _ _ pf in _ = z
           return match z with
                    | Some t => F t
                    | None => unit
                  end
-          with 
+          with
           | eq_refl => hlist_nth h' (n - length l)
         end
     end eq_refl.
@@ -203,9 +234,9 @@ Section hlist.
         generalize (cast2 (l :: ls) l' (S n)).
         generalize (hlist_nth h n).
         simpl in *.
-        pattern (nth_error ls n). 
+        pattern (nth_error ls n).
         destruct (nth_error ls n); simpl; intros.
-        { clear - e. uip_all. clear - e. 
+        { clear - e. uip_all. clear - e.
           rewrite (UIP_equal e0 e5). reflexivity. }
         { uip_all. clear - e.
           rewrite (UIP_equal e5 e6). reflexivity. } } }
@@ -215,8 +246,8 @@ Section hlist.
     Variable eqv : forall x, type (F x).
 
     Global Instance type_hlist (ls : list iT): type (hlist ls) :=
-    { equal := @equiv_hlist (fun x => @equal _ (eqv x)) ls 
-    ; proper := 
+    { equal := @equiv_hlist (fun x => @equal _ (eqv x)) ls
+    ; proper :=
       (fix recur ls (h : hlist ls) : Prop :=
         match h with
           | Hnil => True
@@ -225,7 +256,7 @@ Section hlist.
     }.
 
     Variable eqvOk : forall x, typeOk (eqv x).
-    Variable ED : EquivDec.EqDec _ (@eq iT).    
+    Variable ED : EquivDec.EqDec _ (@eq iT).
 
     Global Instance typeOk_hlist (ls : list iT): typeOk (type_hlist ls).
     Proof.
@@ -244,19 +275,20 @@ Section hlist.
         { rewrite (hlist_eta x); intros; intuition; constructor.
           eapply preflexive; eauto with typeclass_instances.
           eapply IHls; eauto. } }
-      { red. induction ls; intros; 
-        rewrite (hlist_eta x) in *; rewrite (hlist_eta y) in *; simpl in *; intros; auto.
+      { red. induction ls; intros;
+        rewrite (hlist_eta x) in *; rewrite (hlist_eta y) in *; simpl in *;
+        intros; auto.
         inversion H. subst. inv_all; subst.
         constructor; auto. symmetry. eauto. }
-      { red. induction ls; intros; 
-        rewrite (hlist_eta x) in *; rewrite (hlist_eta y) in *; rewrite (hlist_eta z) in *; 
-          simpl in *; intros; auto. 
+      { red. induction ls; intros;
+        rewrite (hlist_eta x) in *; rewrite (hlist_eta y) in *;
+        rewrite (hlist_eta z) in *; simpl in *; intros; auto.
         inversion H. inversion H0. subst; inv_all; subst.
         constructor.
         { etransitivity; eauto. }
         { eapply IHls; eauto. } }
     Qed.
-    
+
     Global Instance proper_hlist_app l l' : proper (@hlist_app l l').
     Proof.
       do 6 red. induction x; intros;
@@ -283,10 +315,9 @@ Section hlist_map.
   Fixpoint hlist_map (ls : list A) (hl : hlist F ls) {struct hl} : hlist G ls :=
     match hl in @hlist _ _ ls return hlist G ls with
       | Hnil => Hnil
-      | Hcons _ _ hd tl => 
+      | Hcons _ _ hd tl =>
         Hcons (ff hd) (hlist_map tl)
     end.
 End hlist_map.
 
 Arguments hlist_map {_ _ _} _ {_} _.
-
