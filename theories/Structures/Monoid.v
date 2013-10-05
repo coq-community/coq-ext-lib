c45c4d4f226edef76264a7f7761c011bccecf35e
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-10-04 23:23:23 -0400

    adding a very weak Foldable law.

--- theories/Structures/Monoid.v
+++ theories/Structures/Monoid.v
@@ -1,3 +1,6 @@
+Require Import ExtLib.Core.Type.
+Require Import ExtLib.Structures.BinOps.
+
 Set Implicit Arguments.
 Set Maximal Implicit Insertion.
 
@@ -7,6 +10,14 @@ Section Monoid.
   Record Monoid : Type :=
   { monoid_plus : S -> S -> S
   ; monoid_unit : S
+  }.
+
+  Context {Type_S : type S}.  Check @equal.
+
+  Class MonoidLaws (M : Monoid) : Type :=
+  { monoid_assoc :> Associative M.(monoid_plus) equal
+  ; monoid_lunit :> LeftUnit M.(monoid_plus) M.(monoid_unit) equal
+  ; monoid_runit :> RightUnit M.(monoid_plus) M.(monoid_unit) equal
   }.
 
 End Monoid.
