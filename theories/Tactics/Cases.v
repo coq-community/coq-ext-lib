6770ddf789e836f0123b394da6c339f8663c8a89
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-10-28 09:51:50 -0400

    a few more tactics for doing case analysis.

--- theories/Tactics/Cases.v
+++ theories/Tactics/Cases.v
@@ -3,7 +3,12 @@ Require Import ExtLib.Tactics.Consider.
 Set Implicit Arguments.
 Set Strict Implicit.
 
-Ltac forward :=
+(** This tactic will perform case splits on terms that
+ ** are matched on. It only does this on terms where only
+ ** one of the cases is non-trivial (i.e. by [intuition congruence]).
+ **
+ **)
+Ltac forward' dst sol :=
   let check X :=
     match X with
       | match _ with _ => _ end => fail 1
@@ -11,11 +16,55 @@ Ltac forward :=
       | _ => idtac
     end
   in
+  let go X :=
+    first [ (dst X; try solve [ sol ]); [ intros ]
+          | dst X; solve [ sol ] ]
+  in
   repeat match goal with
            | [ H : context [ match ?X with _ => _ end ] |- _ ] =>
-             check X; 
-             (consider X; try congruence); [ intros ]
+             go X
            | [ H : context [ if ?X then _ else _ ] |- _ ] =>
-             check X; 
-             (consider X; try congruence); [ intros ]
+             go X
+           | [ |- context [ match ?X with _ => _ end ] ] =>
+             go X
+           | [ |- context [ if ?X then _ else _ ] ] =>
+             go X
+         end.
+
+Ltac forward :=
+  forward'
+    ltac:(fun x => consider x; intros)
+    ltac:(intuition congruence).
+
+Ltac forward_unsafe' dst sol :=
+  let check X :=
+    match X with
+      | match _ with _ => _ end => fail 1
+      | if _ then _ else _ => fail 1
+      | _ => idtac
+    end
+  in
+  let go X :=
+      dst X; try solve [ sol ]
+  in
+  repeat match goal with
+           | [ H : context [ match ?X with _ => _ end ] |- _ ] =>
+             go X
+           | [ H : context [ if ?X then _ else _ ] |- _ ] =>
+             go X
+           | [ |- context [ match ?X with _ => _ end ] ] =>
+             go X
+           | [ |- context [ if ?X then _ else _ ] ] =>
+             go X
+         end.
+
+Ltac forward_unsafe :=
+  forward_unsafe'
+    ltac:(fun x => consider x; intros)
+    ltac:(intuition congruence).
+
+Ltac rewrite_all :=
+  repeat match goal with
+           | [ H : _ |- _ ] =>
+             erewrite H by eauto with typeclass_instances
          end.
