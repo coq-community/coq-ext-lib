d93ba5d5a8b9f0e0b602887c4a4cd61852e19cc7
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-12-18 15:59:05 -0500

    implicit arguments on mjoin

--- theories/Structures/MonadPlus.v
+++ theories/Structures/MonadPlus.v
@@ -6,12 +6,12 @@ Set Maximal Implicit Arguments.
 Class MonadPlus (m : Type -> Type) : Type :=
 { mplus : forall {A B:Type}, m A -> m B -> m (A + B)%type }.
 
-Definition mjoin (m : Type -> Type) (M : Monad m) (MP : MonadPlus m) T (a b : m T) : m T :=
+Definition mjoin {m : Type -> Type} {M : Monad m} {MP : MonadPlus m} {T} (a b : m T) : m T :=
   bind (mplus a b) (fun x =>
     match x with
       | inl x | inr x => ret x
     end).
 
 Module MonadPlusNotation.
-  Notation "x <+> y" := (mplus x y) (at level 49, right associativity) : monad_scope.
+  Notation "x <+> y" := (@mplus _ _ _ _ x y) (at level 49, right associativity) : monad_scope.
 End MonadPlusNotation.
