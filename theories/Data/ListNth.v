b2303cddfd74970216e6b728a9d8776f15e0b40c
Gregory Malecha <gmalecha@cs.harvard.edu>
2014-06-10 13:15:48 -0400

    Adding nth_error_length_lt
    = Fixing imports

--- theories/Data/ListNth.v
+++ theories/Data/ListNth.v
@@ -1,5 +1,5 @@
-Require Import List.
-Require Import Omega.
+Require Import Coq.Lists.List.
+Require Import Coq.omega.Omega.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -58,6 +58,17 @@ Section parametric.
     induction ls; destruct n; simpl in *; auto; simpl in *; try omega.
     inversion 1. intro. apply IHls in H. omega.
   Qed.
+
+  Lemma nth_error_length_lt : forall {T} (ls : list T) n val,
+    nth_error ls n = Some val -> n < length ls.
+  Proof.
+    induction ls; destruct n; simpl; intros; auto.
+    { inversion H. }
+    { inversion H. }
+    { omega. }
+    { apply Lt.lt_n_S. eauto. }
+  Qed.
+
 
   Theorem nth_error_map : forall U (f : T -> U) ls n,
     nth_error (map f ls) n = match nth_error ls n with
