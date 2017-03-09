e065b1304a59e9dd7c627ee3c9c0a04753257ed4
Vadim Zaliva <lord@crocodile.org>
2017-03-09 14:29:07 -0800

    CoMonadLaws class

---
+++ theories/Structures/CoMonadLaws.v
@@ -0,0 +1,25 @@
+Require Import Coq.Program.Basics.
+Require Import ExtLib.Structures.CoMonad.
+
+Set Implicit Arguments.
+Set Strict Implicit.
+
+Local Open Scope program_scope.
+
+Section CoMonadLaws.
+  Variable m : Type -> Type.
+  Variable C : CoMonad m.
+
+  Class CoMonadLaws : Type :=
+    {
+      extend_extract: forall (A B:Type),
+        extend (B:=A) extract = id ;
+
+      extract_extend: forall (A B:Type) {f},
+          extract ∘ extend (A:=A) (B:=B) f = f;
+
+      extend_extend:forall (A B:Type) {f g},
+          extend (A:=B) (B:=A) f ∘ extend (A:=A) g = extend (f ∘ extend g)
+    }.
+
+End CoMonadLaws.
