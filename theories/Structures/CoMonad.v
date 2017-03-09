cfd0fc3fa5c8e36747ad387da02a594d05dbce99
Vadim Zaliva <lord@crocodile.org>
2017-03-09 14:20:25 -0800

    renamed CoMonad functoins coret and cobind to match names (extract,extend) and argument order used in Haskell and other places

--- theories/Structures/CoMonad.v
+++ theories/Structures/CoMonad.v
@@ -2,6 +2,6 @@ Set Implicit Arguments.
 Set Strict Implicit.
 
 Class CoMonad (m : Type -> Type) : Type :=
-{ coret : forall {A}, m A -> A
-; cobind : forall {A B}, m A -> (m A -> B) -> m B
+{ extract : forall {A}, m A -> A
+; extend : forall {A B}, (m A -> B) -> m A -> m B
 }.
