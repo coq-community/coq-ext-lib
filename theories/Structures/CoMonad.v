f1791368938ba1619dff710baddf2aab666ecbbe
Vadim Zaliva <lord@crocodile.org>
2019-05-26 10:36:40 -0700

    `coret` and `cobind` aliases

--- theories/Structures/CoMonad.v
+++ theories/Structures/CoMonad.v
@@ -5,3 +5,14 @@ Class CoMonad (m : Type -> Type) : Type :=
 { extract : forall {A}, m A -> A
 ; extend : forall {A B}, (m A -> B) -> m A -> m B
 }.
+
+(* Aliases for [extract] and [extend] for backward compatiblity *)
+Section BackwardCompatibility.
+  Context {m: Type->Type}.
+  Context {CoMonad: CoMonad m}.
+
+  Definition coret {A: Type} := extract (A:=A).
+  Definition cobind {A B: Type} := extend (A:=A) (B:=B).
+
+End BackwardCompatibility.
+
