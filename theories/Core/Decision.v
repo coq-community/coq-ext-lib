05533df519a9a8a5c42888fc325a4f9e365cd486
Abhishek Anand (optiplex7010@home) <abhishek.anand.iitg@gmail.com>
2020-01-07 13:00:43 -0800

    generic stuff from squiggleeq: fwd tactics and decision helpers

---
+++ theories/Core/Decision.v
@@ -0,0 +1,26 @@
+Require Import Coq.Classes.DecidableClass.
+Definition decideP (P:Prop) `{Decidable P} : {P} + {~P}.
+  remember (decide P) as dec. 
+  destruct dec; [left | right];
+  symmetry in Heqdec;
+  unfold decide in Heqdec;
+  pose proof Decidable_spec; try tauto.
+  intro Hc.
+  rewrite <- H0 in Hc. congruence.
+Defined.
+
+Ltac cases_ifd Hn :=
+  match goal with
+      [ |- context[if ?d then ?tt else ?ff] ]
+      => let Hnt := fresh Hn "t" in
+         let Hnf := fresh Hn "f" in
+         destruct d as [Hnt | Hnf] end.
+
+Lemma decide_decideP  {P:Prop }`{Decidable P} {R:Type} (a b : R) :
+(if (decide P) then  a else b) = (if (decideP P) then  a else b).
+Proof.
+  symmetry. unfold decide.
+  destruct (decideP P).
+- rewrite Decidable_complete; auto.
+- rewrite Decidable_sound_alt; auto.
+Qed.
--- theories/Tactics/Forward.v
+++ theories/Tactics/Forward.v
@@ -16,3 +16,62 @@ Ltac forward_reason :=
                clear H'
              end
          end.
+
+(* based on a 2013 version of software foundations/libtatics *)
+Definition ltac_something (P:Type) (e:P) := e.
+
+Notation "'Something'" := 
+  (@ltac_something _ _).
+
+Lemma ltac_something_eq : forall (e:Type),
+  e = (@ltac_something _ e).
+Proof. auto. Qed.
+
+Lemma ltac_something_hide : forall (e:Type),
+  e -> (@ltac_something _ e).
+Proof. auto. Qed.
+
+Lemma ltac_something_show : forall (e:Type),
+  (@ltac_something _ e) -> e.
+Proof. auto. Qed.
+
+Ltac show_hyp H :=
+  apply ltac_something_show in H.
+
+Ltac hide_hyp H :=
+  apply ltac_something_hide in H.
+
+Ltac show_hyps :=
+  repeat match goal with
+    H: @ltac_something _ _ |- _ => show_hyp H end.
+
+Ltac rwHyps :=
+  repeat match goal with
+           [ H: _ = _ |- _] =>  repeat rewrite -> H; hide_hyp H
+         end; show_hyps.
+
+Ltac rwHypsR :=
+  repeat match goal with
+           [ H: _ = _ |- _] =>  repeat rewrite <- H; hide_hyp H
+         end; show_hyps.
+
+Ltac rwHypsA :=
+  repeat match goal with
+           [ H: _ = _ |- _] =>  repeat rewrite -> H in *; hide_hyp H
+         end; show_hyps.
+
+Ltac rwHypsRA :=
+  repeat match goal with
+           [ H: _ = _ |- _] =>  repeat rewrite <- H in *; hide_hyp H
+         end; show_hyps.
+
+(* based on a tactic written by Vincent Rahli *)
+Ltac clear_trivials :=
+  repeat match goal with
+           | [ H : ?T = ?T |- _ ] => clear H
+           | [ H : ?T <-> ?T |- _ ] => clear H
+           | [ H : ?T -> ?T |- _ ] => clear H
+           | [ H1 : ?T, H2 : ?T |- _ ] => clear H2
+           | [ H : True |- _ ] => clear H
+           | [ H : not False |- _ ] => clear H
+         end.
