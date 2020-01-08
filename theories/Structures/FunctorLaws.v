f258f2763c64408ea58a0e7503253efca6173760
Yishuai Li <yishuai@upenn.edu>
2020-01-07 19:43:14 -0500

    Uncomment MonadLaws

--- theories/Structures/FunctorLaws.v
+++ theories/Structures/FunctorLaws.v
@@ -8,14 +8,9 @@ Set Universe Polymorphism.
 
 Section laws.
 
-  Class FunctorLaws@{t u X}
-              (F : Type@{t} -> Type@{u})
-              (Functor_F : Functor F)
-  : Type :=
-  { fmap_id : forall T (x : F T), fmap id x = x
-  ; fmap_compose : forall (T U V : Type@{t})
-                     (x : F T),
-      forall (f : T -> U) (g : U -> V),
+  Class FunctorLaws {F} (Functor_F : Functor F) :=
+  { fmap_id : forall {T} (x : F T), fmap id x = x
+  ; fmap_compose : forall {T U V} (f : T -> U) (g : U -> V) (x : F T),
         fmap (compose g f) x = fmap g (fmap f x)
   }.
 End laws.
