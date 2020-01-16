70d2f8c732f7cfc8dbb9c5025877b4b2878960b0
Abhishek Anand (optiplex7010@home) <abhishek.anand.iitg@gmail.com>
2020-01-16 08:38:48 -0800

    comment about borrowing names from SF and the (dis)similarities in
    the implementation

--- theories/Tactics/Hide.v
+++ theories/Tactics/Hide.v
@@ -1,6 +1,13 @@
-(* caution: hiding a non-proof may be irreversible. We can change
+(* The names of the tactics here come from the 2013 distribution 
+of Software Foundations (SF). The implementations are different, though:
+1) The hiding mechanism is SF was not fully reliable:
+ tactics that compute on hypotheses could unhide stuff
+2) Hiding in SF was always reversible. Here, unhiding only works
+when proving a Prop. We can change
 the return type to [Type]. But then hiding hypothesis can make
-things unprovable by introducing universe constraints *)
+things unprovable by introducing universe constraints.
+*)
+
 Inductive Hidden (P:Type) : Prop:=
 | hidden (p:P): Hidden P.
 
