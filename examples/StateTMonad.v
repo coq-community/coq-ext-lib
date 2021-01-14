6da63d8d9a1f956e88f1d8af921969a4af89c440
Yishuai Li <liyishuai@users.noreply.github.com>
2021-01-14 08:46:17 -0500

    Apply suggestions from code review
    Coqdocify comments

--- examples/StateTMonad.v
+++ examples/StateTMonad.v
@@ -3,13 +3,13 @@ From ExtLib Require Import
      OptionMonad
      StateMonad.
 
-(* Monad_stateT is not in context, so this definition fails *)
+(** [Monad_stateT] is not in context, so this definition fails *)
 Fail Definition foo : stateT unit option unit :=
   ret tt.
 
-(* Use Existing Instance to bring the Local Monad_stateT instance into context *)
+(** Use [Existing Instance] to bring the Local [Monad_stateT] instance into context *)
 Existing Instance Monad_stateT.
 
-(* Now the definition succeeds *)
+(** Now the definition succeeds *)
 Definition foo : stateT unit option unit :=
   ret tt.
